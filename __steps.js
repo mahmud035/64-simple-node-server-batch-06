0906ba50c7f6faa7baff088a2418fe1fd5ccd100
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-28 23:29:16 +0600

    64-3 Create React form and Post API and send data to the server

---
+++ __steps.js
@@ -0,0 +1,10 @@
+/* 
+//* POST
+1. Create post API
+2. Use fetch() with method POST
+3. add headers to fetch(): 'Content-Type': 'application/json'
+4. add body to fetch() to send data to the server
+5. Make sure to use JSON.stringify() to send data
+6. Important: Use express.json() as middleware
+
+*/
--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
-
 const Port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Simple Node Server Running');
+});
+
+//* middleware
+app.use(cors());
+app.use(express.json());
+
+const users = [
+  { id: 1, name: 'John', email: 'john@gmail.com' },
+  { id: 2, name: 'smith', email: 'smith@gmail.com' },
+  { id: 3, name: 'alex', email: 'alex@gmail.com' },
+];
+
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+app.post('/users', (req, res) => {
+  console.log('Post API called');
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+
+  console.log(user);
+  res.send(user);
 });
 
 app.listen(Port, () => {
